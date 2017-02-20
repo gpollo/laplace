7fa88e848eaa93c52de7c99e21e9315fd817247e
Gabriel-Andrew Pollo Guilbert <gabriel14_wii@hotmail.com>
2017-02-20 09:17:28 -0500

    Update main.c example

--- src/main.c
+++ src/main.c
@@ -14,20 +14,9 @@ struct bound* boundaries(struct bound* bound, struct rect* pos) {
     } else if(48 <= x && x <= 64 && y <= 0) {
         bound->value = -150;
         bound->cond  = DIRICHLET;
-    } else if(16 <= x && x< 48) {
-        if(12 < y && y <= 36) {
-            bound->value = 0;
-            bound->cond  = NONE;
-        } else {
-            bound->value = 0;
-            bound->cond  = NEUMANN;
-        }
-//    } else if((x-10)*(x-10)+(y-20)*(y-30)<16) {
-//            bound->value = 0;
-//            bound->cond  = NEUMANN;
-//    } else if((x-50)*(x-50)+(y-25)*(y-25) < 9) {
-//            bound->value = -150;
-//            bound->cond  = DIRICHLET;
+    } else if(16 <= x && x< 48 && (y < 12 || y >= 36)) {
+        bound->value = 0;
+        bound->cond  = NEUMANN;
     } else {
         bound->value = 0;
         bound->cond  = NONE;
@@ -39,18 +28,14 @@ struct bound* boundaries(struct bound* bound, struct rect* pos) {
 int main() {
     struct rect size = {64, 48};
     struct point dim = {160, 120};
-    struct config conf = {8, 50, 0.5};
+    struct config conf = {8, 10, 0.01};
     struct lattice* lattice = lattice_new(&size, &dim, &boundaries);
     struct worker* worker;
 
 //    printf("%d\n", lattice_compute(lattice, 0.005));
-   printf("%d\n", lattice_compute_threaded(lattice, &conf));
- //   printf("%d\n", lattice_compute(lattice, 0.01));
+    printf("%d\n", lattice_compute_threaded(lattice, &conf));
 
-   // worker = worker_new(NULL, lattice);
-   // if(worker != NULL) pthread_join(worker->thread, NULL);
-
- //   lattice_print(lattice);
+    lattice_print(lattice);
     lattice_delete(lattice);
 
 	return 0;
